11ad3eacb52dd5fae7dcb77fa98662cf934526a5
yoon5450 <dbseodnd356@naver.com>
2025-06-17 20:28:55 +0900

    feat: html skeleton

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,9 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+  "rules": {
+    "no-unused-vars": "warn"
+  }
+}
 ]);
