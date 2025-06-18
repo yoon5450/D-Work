e1f46472f50d6d05b2128f389ae51e895d8bb14a
Young-mang <fgb9763@gmail.com>
2025-06-19 07:13:13 +0900

    feat: 관심공고,정렬,목록렌더 이벤트

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
-let loginBtn = document.querySelector('#login-btn')
-let overlay = document.querySelector('#overlay')
+import {
+  postRender,
+  getNode,
+  handleBookmarkBtnClick,
+  handleSort,
+  
+} from './jung/lib/index.js';
 
-window.addEventListener('load', (e) => {
-  overlay.classList.add('hidden')
-})
+import dummyJobPostings from './jung/data/dummyJobPostingsData.js';
+import dummyUserData from './jung/data/dummyUserData.js';
 
-loginBtn.addEventListener('click', (e) => {
-  overlay.classList.remove('hidden')
-})
+/*
+1. 데이터 가져와서 데이터 선언하기
+2. 템플렛({company,position,stack,experience,location,type})
+3. render(){postListTemplate()}
+4. 오름차순 내림차순 정렬
+    - makeSortList(정렬기준){}
+    - data.entries [id: value],[정렬기준:value]
+    - 새로운 데이터 배열 만들기 id value 에 정렬기준 value
+    - 새로운 데이터 배열 오름차순 내림차순 생성
+    - 해당 배열들을 이용해서 render
+5. 관심공고 버튼 클릭 이벤트
+6. 정렬기준 선택 클릭 이벤트
 
-document.getElementById('overlay').addEventListener('click', (e) => {
-  e.currentTarget.classList.add('hidden')
-})
+페이지 전환
+    -SPA
+*/
 
-document.querySelector('.login-modal').addEventListener('click', (e) => {
-  e.stopPropagation()
-})
 
-document.querySelector('.close-btn').addEventListener('click', (e) => {
-  let overlay = document.querySelector('#overlay')
-  overlay.classList.add('hidden')
-})
+const data= dummyJobPostings()
+// console.log(data);
+const userData = dummyUserData()
+
+
+//관심공고버튼 클릭 이벤트
+getNode('tbody').addEventListener("click",handleBookmarkBtnClick)
+
+//첫화면
+window.addEventListener('load',postRender(data));
+
+
+
+//정렬버튼 클릭 이벤트
+getNode("thead").addEventListener('click',handleSort);
