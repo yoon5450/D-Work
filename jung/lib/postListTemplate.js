eda775b5fc4aa09a42528651fd40e332b05d5c3c
Young-mang <fgb9763@gmail.com>
2025-06-19 00:03:34 +0900

    feat: 공고 리스트 템플렛 생성

---
+++ jung/lib/postListTemplate.js
@@ -0,0 +1,32 @@
+
+
+// 공고 리스트 템플렛 생성
+export function postListTemplate(options){
+    const {
+        company,
+        position,
+        experience,
+        location,
+        type,
+        stack,
+        companyscore
+    }=options
+    return `
+    <tr>
+              <td>${company}</td>
+              <td>${position}</td>
+              <td>${experience}</td>
+              <td>${location}</td>
+              <td>${type}</td>
+              <td>${stack}</td>
+              <td>${companyscore}</td>
+              <td>
+								<div class="main-table-btn-wrapper">
+                  <button class="main-table-apply-btn">지원하기</button>
+                  <button class="main-table-bookmark-btn">관심목록 추가</button>
+                </div>
+            	</td>
+            </tr>
+    `
+}
+
