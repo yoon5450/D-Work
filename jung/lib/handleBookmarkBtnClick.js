0bdbc408eac8e085d6fce9e325ec07ffd72d37ca
Young-mang <fgb9763@gmail.com>
2025-06-19 23:18:19 +0900

    feat: bookmarklist 데이터 로컬 스토리지 연결

--- jung/lib/handleBookmarkBtnClick.js
+++ jung/lib/handleBookmarkBtnClick.js
@@ -1,37 +1,34 @@
 import dummyJobPostings from '../data/dummyJobPostingsData.js';
 import dummyUserData from '../data/dummyUserData.js';
+import { getStorage, setStorage } from './utils/storage.js';
 
 
 //원본훼손문제
 //유저 데이터 어케 가지고옴?
 
-
-const data= dummyJobPostings()
 const userData = dummyUserData()
+setStorage(userData)
+
 
 function addBookmark(userIndex=0, id){
-  userData[userIndex].bookmarklist.push(id)
+  // userData[userIndex].bookmarklist.push(id)
   //userBoolmarkList가 훼손되는데... 근데 지금으로는 이렇게 안하면 추가가 안되요
-  /*
-  나중에 데이터에서 get/set한다면
-  let copy=[...userData[userIndex].bookmarklist]
-  set copy.push(id)
-  */
-  return userData[userIndex].bookmarklist
+  const data = getStorage();
+  let userBookmarklist = data[userIndex].bookmarklist;
+  if (!userBookmarklist.includes(id)) {
+    userBookmarklist.push(id);
+  }
+  setStorage(data);
 }
 
 function removeBookmark(userIndex=0,id){
-  /*
-  const userBookmarkList=userData[userIndex].bookmarklist
-  let updated= userBookmarkList.filter((num)=>num!==id);
-  */
-    //이것도 원본훼손
-    const index = userData[userIndex].bookmarklist.indexOf(id);
-    if (index !== -1) {
-  userData[userIndex].bookmarklist.splice(index, 1); // index부터 1개 삭제
-    }
+  
+  const data = getStorage();
+  let userBookmarklist = data[userIndex].bookmarklist;
 
-  return userData[userIndex].bookmarklist
+  data[userIndex].bookmarklist = userBookmarklist.filter((num) => num !== id);
+
+  setStorage(data);
 }
 
 
@@ -44,14 +41,17 @@ export function handleBookmarkBtnClick(e){
   e.preventDefault();
   const id=+getPostId(e);
   let userIndex=0; //유저데이터 인덱스 어떻게 가져와요?
-  let target=e.target.closest('.main-table-bookmark-btn')||e.target.closest('.main-table-bookmark-btn activate')
+  let target=e.target.closest('.main-table-bookmark-btn')
   
-  if(!(userData[userIndex].bookmarklist.includes(id))){
+  const currentData = getStorage();
+  console.log(currentData);
+  
+  if(!(currentData[userIndex].bookmarklist.includes(id))){
     addBookmark(userIndex,id);
     target.classList.add('activate');
     target.textContent='내 관심 공고'
     
-  }else if(userData[userIndex].bookmarklist.includes(id)){
+  }else if(currentData[userIndex].bookmarklist.includes(id)){
     removeBookmark(userIndex,id);
     target.classList.remove('activate');
     target.textContent='관심목록 추가'
