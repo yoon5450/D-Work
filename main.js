8192eee57dc0133b30a83923d9fb168c2ac7aad4
yoon5450 <dbseodnd356@naver.com>
2025-06-20 08:52:20 +0900

    fix: 회원가입 API링크 수정,  CORS 수정

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 import { renderCalendar } from "./kim/lib/renderCalendar.js";
+import { reqLogin, reqSignIn } from "./lib/index.js";
 
 let loginBtn = document.querySelector('#login-btn')
+let overlayLoginBtn = document.querySelector('.overlay-btn-login')
+let overlayUsernameInput = document.querySelector('#overlay-username');
+let overlayUserpassInput = document.querySelector('#overlay-userpass');
 let overlay = document.querySelector('#overlay')
+
 
 window.addEventListener('load', (e) => {
   overlay.classList.add('hidden')
 })
 
-loginBtn.addEventListener('click', (e) => {
+loginBtn.addEventListener('click', (e) =>{
   overlay.classList.remove('hidden')
+})
+
+overlayLoginBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const username = overlayUsernameInput;
+  const pass = overlayUserpassInput;
+  let response = await reqLogin(username, pass)
+  console.log(response.msg)
+  
 })
 
 document.getElementById('overlay').addEventListener('click', (e) => {
